65f90b21b108b7c6698e93f4b034a404a67344ca
travbula@protonmail.com <travbula@protonmail.com>
2022-06-04 12:02:08 +0000

    Add distance segment for races

--- models/marts/core/dim_race.sql
+++ models/marts/core/dim_race.sql
@@ -21,7 +21,15 @@ final as (
         race_program.start_method,
         race_result.track_state,
         race_stats.number_of_horses_to_start,
-        race_stats.total_prize_pool
+        race_stats.total_prize_pool,
+        case
+            when race_program.distance < 1500 then 'ultra sprint'
+            when race_program.distance >= 1500 and race_program.distance < 2000 then 'sprint'
+            when race_program.distance >= 2000 and race_program.distance < 2200 then 'regular'
+            when race_program.distance >= 2200 and race_program.distance < 3000 then 'long'
+            when race_program.distance >= 3000 then 'ultra long'
+            else 'unknown'
+        end as distance_segment
 
     from race_program
     inner join race_stats on race_program.race_id = race_stats.race_id -- left join instead? (this results in some null values because some complete_results are missing)
